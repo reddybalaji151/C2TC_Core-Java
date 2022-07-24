9b1dccf7438eb652f226b5df4462bebd5b3888f5
Reddappa <Reddappa@DESKTOP-JN0H6VS>
2022-07-24 14:52:44 +0530

    first commit

---
+++ Acharaya_day02/src/ConditionalOperator.java
@@ -0,0 +1,16 @@
+
+public class ConditionalOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int value1 = (a<b)?a:b;
+		int value2 = (a>b)?a:b;
+		System.out.println(value1);
+		System.out.println(value2);
+		
+
+	}
+
+}
